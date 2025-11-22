a1cc71468d17feb4dcc3513777c015994114728f
hankkuu <hankkuu@gmail.com>
2025-11-22 14:26:02 +0900

    task-5: swagger-ui 의존성 추가

--- api-server/build.gradle.kts
+++ api-server/build.gradle.kts
@@ -9,6 +9,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+    // Swagger/OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.13.8")
